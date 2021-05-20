b3e4fbe954023b414a0023ee08cad6fe9a51a48a
jgrifell <jgrifell@werfen.com>
2021-05-20 15:29:52 +0200

    first commit

--- CustomSecurityRealm/SingletonEJB/src/main/java/com/grifell/pep/BatchManagerBean.java
+++ CustomSecurityRealm/SingletonEJB/src/main/java/com/grifell/pep/BatchManagerBean.java
@@ -1,2 +1,20 @@
-package com.grifell.pep;public class BatchManagerBean {
+package com.grifell.pep;
+
+import javax.annotation.security.RunAs;
+import javax.ejb.LocalBean;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Singleton
+@LocalBean
+@Startup
+@RunAs("SYSTEM")
+public class BatchManagerBean {
+
+    @Schedule(dayOfWeek = "*", hour = "0", persistent = false)
+    public void cleanTimeout() {
+        //calls a securized EJB
+    }
+
 }
